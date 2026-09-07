51713632e63dac96a9277e3e714360d89f7276ce
ArturoSojo <120525560+ArturoSojo@users.noreply.github.com>
2026-09-07 14:59:06 -0400

    Update endCounter value from 2 to 4

--- data.tsx
+++ data.tsx
@@ -82,7 +82,7 @@ export const dataAboutPage = [
 export const dataCounter = [
     {
         id: 0,
-        endCounter: 2,
+        endCounter: 4,
         text: "Años de experiencia",
         lineRight: true,
         lineRightMobile: true,
